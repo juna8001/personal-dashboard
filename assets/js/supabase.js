4142c731bc090d87372b036453a8d010dc02cb2e
Kamil <juna8001@gmail.com>
2026-08-24 17:33:11 +0200

    test

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = 'https://mwppdqqnorccyzymsagn.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_E8T0rerP4Fn_yo-r_5IIJw_yDfIu0Wg';
 
-const supabase = window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
